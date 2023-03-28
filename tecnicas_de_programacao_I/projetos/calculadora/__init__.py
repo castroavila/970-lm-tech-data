143da095e514bb97b79330fa419cefbe9c3af17a
Manuel Castro <castroavila_2004@hotmail.com>
2023-03-27 21:45:36 -0300

    Initial skeleton of files.

---
+++ tecnicas_de_programacao_I/projetos/calculadora/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @file	   	   : __init__.py
+# @created	   : 27-Mar-2023
+
+"""
+
+"""
---
+++ tecnicas_de_programacao_I/projetos/calculadora/funcoes.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @file	   	   : funcoes.py
+# @created	   : 27-Mar-2023
+
+"""
+
+"""
